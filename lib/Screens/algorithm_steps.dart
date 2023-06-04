37d43813a0461a64f870f560d2376f648211c2e3
Crisiroid <csiroid@gmail.com>
2023-06-05 00:25:59 +0430

    prim algorithm added

--- lib/Screens/algorithm_steps.dart
+++ lib/Screens/algorithm_steps.dart
@@ -1,19 +1,146 @@
 import 'package:flutter/material.dart';
 
-class ShowSteps extends StatelessWidget {
-  final String algorithmName;
-  final List<String> steps;
-  final String result;
-  const ShowSteps(
-      {super.key,
-      required this.algorithmName,
-      required this.steps,
-      required this.result});
+class Edge {
+  final String source;
+  final String destination;
+  final int weight;
+
+  Edge(this.source, this.destination, this.weight);
+}
+
+class Graph {
+  final List<String> nodes;
+  final Map<String, List<Edge>> adjacencyList;
+
+  Graph(this.nodes) : adjacencyList = {for (var node in nodes) node: []};
+
+  void addEdge(String source, String destination, int weight) {
+    final edge1 = Edge(source, destination, weight);
+    final edge2 = Edge(destination, source, weight);
+    adjacencyList[source]!.add(edge1);
+    adjacencyList[destination]!.add(edge2);
+  }
+
+  List<Edge> primMST() {
+    final List<bool> visited = List<bool>.filled(nodes.length, false);
+    final List<Edge> mst = [];
+    final List<int> key = List<int>.filled(nodes.length, 999999);
+    final List<String> parent = List<String>.filled(nodes.length, '');
+
+    key[0] = 0; // Start with the first node
+
+    for (int count = 0; count < nodes.length - 1; count++) {
+      final u = _minKey(key, visited);
+      visited[u] = true;
+
+      for (final edge in adjacencyList[nodes[u]]!) {
+        final v = nodes.indexOf(edge.destination);
+
+        if (!visited[v] && edge.weight < key[v]) {
+          parent[v] = nodes[u];
+          key[v] = edge.weight;
+        }
+      }
+    }
+
+    for (int i = 1; i < nodes.length; i++) {
+      mst.add(Edge(parent[i], nodes[i], key[i]));
+    }
+
+    return mst;
+  }
+
+  int _minKey(List<int> key, List<bool> visited) {
+    int min = 999999;
+    int minIndex = -1;
+
+    for (int v = 0; v < nodes.length; v++) {
+      if (!visited[v] && key[v] < min) {
+        min = key[v];
+        minIndex = v;
+      }
+    }
+
+    return minIndex;
+  }
+}
+
+class MSTPage extends StatefulWidget {
+  @override
+  _MSTPageState createState() => _MSTPageState();
+}
+
+class _MSTPageState extends State<MSTPage> {
+  final List<String> nodes = ['S', 'A', 'B', 'C', 'D', 'T'];
+  final Graph graph = Graph(['S', 'A', 'B', 'C', 'D', 'T']);
+  List<Edge> mst = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Add edges to the graph
+    graph.addEdge('S', 'A', 7);
+    graph.addEdge('S', 'C', 8);
+    graph.addEdge('A', 'C', 3);
+    graph.addEdge('A', 'B', 6);
+    graph.addEdge('A', 'D', 3);
+    graph.addEdge('B', 'T', 5);
+    graph.addEdge('C', 'C', 2);
+    graph.addEdge('C', 'B', 4);
+    graph.addEdge('D', 'T', 2);
+
+    // Calculate the Minimum Spanning Tree
+    mst = graph.primMST();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [Text(algorithmName + "'s Steps")]),
+      appBar: AppBar(
+        title: Text('Minimum Spanning Tree'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'Steps:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: mst.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(
+                    '${mst[index].source} - ${mst[index].destination} : ${mst[index].weight}',
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'Final MST:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: mst.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(
+                    '${mst[index].source} - ${mst[index].destination} : ${mst[index].weight}',
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,52 +1,20 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unnecessary_new
-
+import 'package:algorithm_generator/Screens/algorithm_steps.dart';
 import 'package:algorithm_generator/Utils/custom_button.dart';
 import 'package:flutter/material.dart';
-import 'package:graphview/GraphView.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var graph = new Graph();
-  FruchtermanReingoldAlgorithm builder = FruchtermanReingoldAlgorithm();
-  @override
-  void initState() {
-    final node = Node.Id(0);
-    final node1 = Node.Id(1);
-    final node2 = Node.Id(2);
-    final node3 = Node.Id(3);
-    final node4 = Node.Id(4);
-    final node5 = Node.Id(5);
-    graph.addEdge(node, node1);
-    graph.addEdge(node, node3);
-  }
+  var vertexes = <Map>{};
 
   @override
   Widget build(BuildContext context) {
     return Placeholder(
       child: Scaffold(
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Expanded(
-              child: InteractiveViewer(
-                constrained: true,
-                minScale: 0.01,
-                maxScale: 1,
-                child: GraphView(
-                  graph: graph,
-                  algorithm: FruchtermanReingoldAlgorithm(),
-                  builder: (Node node) {
-                    var a = node.key?.value as int;
-                    return rectangleWidget(a);
-                  },
-                ),
-              ),
-            ),
             Center(
               child: Container(
                 padding: EdgeInsets.all(7),
@@ -66,7 +34,16 @@ class _HomePageState extends State<HomePage> {
                           fontSize: 25),
                     ),
                     const SizedBox(height: 15),
-                    CustomButton(onPressed: () {}, text: "Prim"),
+                    Builder(builder: (context) {
+                      return CustomButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MSTPage()));
+                          },
+                          text: "Prim");
+                    }),
                     CustomButton(onPressed: () {}, text: "Sollin"),
                     CustomButton(onPressed: () {}, text: "kruskal"),
                   ],
@@ -78,21 +55,4 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-}
-
-Widget rectangleWidget(int a) {
-  return InkWell(
-    onTap: () {
-      print('clicked');
-    },
-    child: Container(
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(4),
-          boxShadow: [
-            BoxShadow(color: Color.fromARGB(255, 46, 67, 84), spreadRadius: 1),
-          ],
-        ),
-        child: Text('Node ${a}')),
-  );
 }
